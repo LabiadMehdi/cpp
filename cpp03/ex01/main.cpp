78be5102599c4864a3b60086e1a01aaef8de19af
LabiadMehdi <labiad.coding@gmail.com>
2026-06-27 03:13:41 +0200

    cpp03 ex01 reajusting output for clarity

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -9,7 +9,8 @@ int main(void)
 	barry.takeDamage(3);
 	barry.beRepaired(1);
 	std::cout << barry.getName() << " " << barry.getEnergyPoints() << " " << barry.getHitPoints() << " " << barry.getAttackDmg() << std::endl;
-
+	
+	std::cout << std::endl << std::endl;
 
 	ScavTrap steve = ScavTrap("steve");
 	std::cout << steve.getName() << " " << steve.getEnergyPoints() << " " << steve.getHitPoints() << " " << steve.getAttackDmg() << std::endl;
