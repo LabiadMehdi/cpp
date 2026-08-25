19bce4c72e96308d1b2619c2b91531affaca30cc
LabiadMehdi <labiad.coding@gmail.com>
2026-08-25 20:48:22 +0200

    wip

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+template<typename T>
+void iter(const T* address, size_t length, void (*f)(const T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(address[i]);
+}
+
+template<typename T>
+void iter(T* address, size_t length, void (*f)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(address[i]);
+}
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,9 +3,36 @@
 #include <ostream>
 #include "iter.hpp"
 
+void    printInt(const int &x)
+{
+    std::cout << x << std::endl;
+}
+
+void    incrementInt(int &x)
+{
+    x++;
+}
+
+void    printString(const std::string &x)
+{
+    std::cout << x << std::endl;
+}
 
 int main( void ) {
 
-    
+    int arr[] = {1, 2, 3, 4, 5};
+    std::cout << "before increment" << std::endl;
+    ::iter(arr, 5, printInt);
+    ::iter(arr, 5, incrementInt);
+    std::cout << "after increment" << std::endl;
+    ::iter(arr, 5, printInt);
+
+    const int arr2[] = {1, 2, 3, 4, 5};
+    std::cout << "const arr" << std::endl;
+    ::iter(arr2, 5, printInt);
+
+    std::string strarr[] = {"hello", "bye", "i said bye"};
+    std::cout << "string array" << std::endl;
+    ::iter(strarr, 3, printString);
     return 0;
 }
