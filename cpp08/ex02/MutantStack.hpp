fd4ea0a0b35a045813a041ee94d0d5654db546eb
El Mehdi Labiad <ellabiad@c3r1p2.42nice.fr>
2026-08-27 13:39:35 +0200

    cpp08 done

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::deque<T>::iterator iterator;
+		
+		iterator begin()	{ return this->c.begin(); }
+		iterator end()		{ return this->c.end(); }
+};
